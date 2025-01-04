a329398e947b0dcbaafe4017d1e1443411c3fae8
Azalmaz22 <Damir2298@yandex.ru>
2025-01-04 19:55:17 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtBxEnter = new System.Windows.Forms.TextBox();
+            this.txtBxEvenNumbers = new System.Windows.Forms.TextBox();
+            this.txtBxOddNumbers = new System.Windows.Forms.TextBox();
+            this.txtBxSumEvenNumbers = new System.Windows.Forms.TextBox();
+            this.txtBxSumOddNumders = new System.Windows.Forms.TextBox();
+            this.txtBxDividers = new System.Windows.Forms.TextBox();
+            this.txtBxSumDividers = new System.Windows.Forms.TextBox();
+            this.txtBxEnterLargeDividers = new System.Windows.Forms.TextBox();
+            this.txtBxLargeDividers = new System.Windows.Forms.TextBox();
+            this.txtBxSumLargeDividers = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(661, 37);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(99, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Показать";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtBxEnter
+            // 
+            this.txtBxEnter.Location = new System.Drawing.Point(157, 38);
+            this.txtBxEnter.Name = "txtBxEnter";
+            this.txtBxEnter.Size = new System.Drawing.Size(450, 22);
+            this.txtBxEnter.TabIndex = 1;
+            this.txtBxEnter.TextChanged += new System.EventHandler(this.txtBxEnter_TextChanged);
+            // 
+            // txtBxEvenNumbers
+            // 
+            this.txtBxEvenNumbers.Location = new System.Drawing.Point(157, 157);
+            this.txtBxEvenNumbers.Name = "txtBxEvenNumbers";
+            this.txtBxEvenNumbers.Size = new System.Drawing.Size(261, 22);
+            this.txtBxEvenNumbers.TabIndex = 2;
+            // 
+            // txtBxOddNumbers
+            // 
+            this.txtBxOddNumbers.Location = new System.Drawing.Point(157, 219);
+            this.txtBxOddNumbers.Name = "txtBxOddNumbers";
+            this.txtBxOddNumbers.Size = new System.Drawing.Size(261, 22);
+            this.txtBxOddNumbers.TabIndex = 3;
+            // 
+            // txtBxSumEvenNumbers
+            // 
+            this.txtBxSumEvenNumbers.Location = new System.Drawing.Point(157, 339);
+            this.txtBxSumEvenNumbers.Name = "txtBxSumEvenNumbers";
+            this.txtBxSumEvenNumbers.Size = new System.Drawing.Size(261, 22);
+            this.txtBxSumEvenNumbers.TabIndex = 4;
+            // 
+            // txtBxSumOddNumders
+            // 
+            this.txtBxSumOddNumders.Location = new System.Drawing.Point(157, 400);
+            this.txtBxSumOddNumders.Name = "txtBxSumOddNumders";
+            this.txtBxSumOddNumders.Size = new System.Drawing.Size(261, 22);
+            this.txtBxSumOddNumders.TabIndex = 5;
+            // 
+            // txtBxDividers
+            // 
+            this.txtBxDividers.Location = new System.Drawing.Point(157, 103);
+            this.txtBxDividers.Name = "txtBxDividers";
+            this.txtBxDividers.Size = new System.Drawing.Size(261, 22);
+            this.txtBxDividers.TabIndex = 6;
+            this.txtBxDividers.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtBxSumDividers
+            // 
+            this.txtBxSumDividers.Location = new System.Drawing.Point(157, 277);
+            this.txtBxSumDividers.Name = "txtBxSumDividers";
+            this.txtBxSumDividers.Size = new System.Drawing.Size(261, 22);
+            this.txtBxSumDividers.TabIndex = 7;
+            // 
+            // txtBxEnterLargeDividers
+            // 
+            this.txtBxEnterLargeDividers.Location = new System.Drawing.Point(775, 103);
+            this.txtBxEnterLargeDividers.Name = "txtBxEnterLargeDividers";
+            this.txtBxEnterLargeDividers.Size = new System.Drawing.Size(296, 22);
+            this.txtBxEnterLargeDividers.TabIndex = 8;
+            // 
+            // txtBxLargeDividers
+            // 
+            this.txtBxLargeDividers.Location = new System.Drawing.Point(775, 157);
+            this.txtBxLargeDividers.Name = "txtBxLargeDividers";
+            this.txtBxLargeDividers.Size = new System.Drawing.Size(296, 22);
+            this.txtBxLargeDividers.TabIndex = 9;
+            // 
+            // txtBxSumLargeDividers
+            // 
+            this.txtBxSumLargeDividers.Location = new System.Drawing.Point(775, 216);
+            this.txtBxSumLargeDividers.Name = "txtBxSumLargeDividers";
+            this.txtBxSumLargeDividers.Size = new System.Drawing.Size(296, 22);
+            this.txtBxSumLargeDividers.TabIndex = 10;
+            this.txtBxSumLargeDividers.TextChanged += new System.EventHandler(this.txtBxSumLargeDividers_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Делители";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Натуральное число";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Четн.делители";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Нечетн. делители";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 277);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Сумма делит.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 16);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Сумма чет.делит.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 403);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(137, 16);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Сумма нечет.делит.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(483, 112);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(141, 16);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Произв.натур.число";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(284, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(177, 16);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Ввод натурального числа";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(442, 160);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(194, 16);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Делит. больше произв.числа";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(529, 216);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(95, 16);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Сумма делит.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 450);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBxSumLargeDividers);
+            this.Controls.Add(this.txtBxLargeDividers);
+            this.Controls.Add(this.txtBxEnterLargeDividers);
+            this.Controls.Add(this.txtBxSumDividers);
+            this.Controls.Add(this.txtBxDividers);
+            this.Controls.Add(this.txtBxSumOddNumders);
+            this.Controls.Add(this.txtBxSumEvenNumbers);
+            this.Controls.Add(this.txtBxOddNumbers);
+            this.Controls.Add(this.txtBxEvenNumbers);
+            this.Controls.Add(this.txtBxEnter);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtBxEnter;
+        private System.Windows.Forms.TextBox txtBxEvenNumbers;
+        private System.Windows.Forms.TextBox txtBxOddNumbers;
+        private System.Windows.Forms.TextBox txtBxSumEvenNumbers;
+        private System.Windows.Forms.TextBox txtBxSumOddNumders;
+        private System.Windows.Forms.TextBox txtBxDividers;
+        private System.Windows.Forms.TextBox txtBxSumDividers;
+        private System.Windows.Forms.TextBox txtBxEnterLargeDividers;
+        private System.Windows.Forms.TextBox txtBxLargeDividers;
+        private System.Windows.Forms.TextBox txtBxSumLargeDividers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+     
+        {
+                
+                var inputStr = txtBxEnter.Text;
+                var naturalNamber = int.Parse(inputStr);
+                var inputStr2 = txtBxEnterLargeDividers.Text;
+                var naturalNamber2 = int.Parse(inputStr2);
+
+                var result = 0;
+                var result_1 = 0;
+                var result_2 = 0;
+                var result_3 = 0;               
+                
+                var dividers = "";
+                var evenNumbers = "";
+                var oddNumbers = "";
+                var largeDividers = "";
+
+                for (int i = 1; i <= naturalNamber; i++)
+                {
+                    if (naturalNamber % i == 0)
+                    {   
+                        dividers = dividers + " " + i;
+                        result = result + i;
+                    }
+                    if (naturalNamber % i == 0 && i % 2 == 0)
+                    {    
+                        evenNumbers = evenNumbers + " " + i;
+                        result_1 = result_1 + i;
+                    }
+                    if (naturalNamber % i == 0 && i % 2 != 0)
+                    {    
+                        oddNumbers = oddNumbers + " " + i;
+                        result_2 = result_2 + i;
+                    }
+                    if (naturalNamber % i == 0 && i > naturalNamber2)
+                    {                   
+                     largeDividers = largeDividers + " " + i;
+                    result_3 = result_3 + i;
+                    }
+
+                }
+            txtBxDividers.Text = dividers;
+            txtBxSumDividers.Text = result.ToString();
+            txtBxEvenNumbers.Text = evenNumbers;           
+            txtBxSumEvenNumbers.Text = result_1.ToString();
+            txtBxOddNumbers.Text = oddNumbers;
+            txtBxSumOddNumders.Text = result_2.ToString();
+            txtBxLargeDividers.Text = largeDividers;
+            txtBxSumLargeDividers.Text = result_3.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBxEnter_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBxSumLargeDividers_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+    
